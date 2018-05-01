a622e03c782fd1e4a694bcd9b39d755dba256719
Karina Kozarova <karinakozarova1303@gmail.com>
2018-05-01 15:31:46 +0300

    added source code

---
+++ Asteroids-Lecture1/Assets/Scripts/FramerateLimiter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FramerateLimiter : MonoBehaviour {
+	public int targetFrameRate = 60;
+	float dt = 0f;
+	// Use this for initialization
+	void Start () {
+		QualitySettings.vSyncCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.targetFrameRate != targetFrameRate){
+			Application.targetFrameRate = targetFrameRate;
+		}
+		dt += (Time.unscaledDeltaTime - dt);
+	}
+
+	void OnGUI() {
+		int w = Screen.width, h = Screen.height;
+		GUIStyle style = new GUIStyle();
+
+		Rect rect = new Rect (0,0, w, h * 2 / 100);
+		style.alignment = TextAnchor.UpperLeft;
+		style.fontSize = h * 2 / 25;
+		style.normal.textColor = new Color(1, 1, 1, 1f);
+		float msec = dt * 1000f;
+		float fps = 1f / dt ;
+		string fpsText = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+		GUI.Label(rect, fpsText, style);
+	}
+}
---
+++ Asteroids-Lecture1/Assets/Scripts/MotionAsteroids.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionAsteroids : MonoBehaviour {
+    public Vector3 halfExtents = new Vector3(6, 0, 4);
+    public Vector3 startPos;
+    public Vector3 target;
+    public float speed = 0.2f;
+    public float rotationSpeed = 180f;
+    public float count = 0.5f;
+    float getRandomValue()
+    {
+        return Random.Range(-6, 0);
+    }
+
+    Vector3 getRandomPositionCoordinates()
+    {
+       int which = Random.Range( 0,4);
+       if(which == 1) return new Vector3(9.0f, 0.0f, 0.0f);
+       if (which == 2) return new Vector3(0.0f, 0.0f, 6.0f);
+       if (which == 3) return new Vector3(6.0f, 0.0f, 4.0f);
+
+    return new Vector3(0.0f, 0.0f, -6.0f);
+    }
+    void Start()
+    {
+        //target = new Vector3(12.0f, 12.0f, 12.0f); 
+        target = getRandomPositionCoordinates();
+        startPos = this.transform.position;
+    }
+    void Update() {
+        float step = speed * Time.deltaTime*2;
+        transform.position = Vector3.MoveTowards(transform.position,target, 0.5f);
+        Vector3 newPosition = transform.position;
+        for (int i = 0; i < 3; ++i)
+        {
+            float prev = newPosition[i];
+            newPosition[i] = Mathf.Clamp(newPosition[i], -halfExtents[i], halfExtents[i]);
+            if (prev != newPosition[i])
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startPos, step);
+                Debug.Log("Start pos: " + startPos);
+            }
+
+        }
+        transform.position = newPosition;
+
+        Update();
+    }
+
+    
+}
