ca038aba3eefd708e06e81b6009c5a0d0894cc26
Karina Kozarova <karinakozarova1303@gmail.com>
2018-05-02 12:08:27 +0300

    add hw

---
+++ HW/Assets/Scripts/AsteroidMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidMovement : MonoBehaviour {
+    public float maxPositionX = 9.8f;
+    public float minPositionX = -9.8f;
+    public float maxPositionY = 5.2f;
+    public float minPositionY = -5.2f;
+    public float speed = 60.0f;
+    public float rotationSpeed = 500f;
+    public Vector3 halfExtents = new Vector3(9.8f, 0, 5.2f);
+    float hor, ver;
+    private void Start()
+    {
+        hor = Random.Range(minPositionX, maxPositionX);
+        ver = Random.Range(minPositionY, maxPositionY);
+    }
+
+    void Update () {
+        
+        Quaternion offset = Quaternion.Euler(0f, 0f, rotationSpeed * Time.deltaTime);
+        transform.rotation = transform.rotation * offset;
+        Vector3 displacement = new Vector3(hor, 0f, ver).normalized * speed * Time.deltaTime;
+        Vector3 newPosition = transform.position + displacement;
+        if (newPosition.x >= maxPositionX || newPosition.x <= minPositionX || newPosition.y >= maxPositionY || newPosition.y <= minPositionY)
+        {
+            hor = Random.Range(minPositionX, maxPositionX);
+            ver = Random.Range(minPositionY,maxPositionY);
+            transform.position = new Vector3(Random.Range(-9.7f, 9.7f), 0f, Random.Range(-5.1f, 5.1f));
+        }
+        else transform.position = newPosition;
+    }
+}
---
+++ HW/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 0.2f;
+    public float rotationSpeed = 180f;
+    public Vector3 halfExtents = new Vector3(6, 0, 4);
+    void Update () {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Quaternion offset = Quaternion.Euler(0f, horizontalInput * rotationSpeed * Time.deltaTime, 0f);
+        transform.rotation = transform.rotation * offset;
+        Vector3 displacement = new Vector3(0f, 0f, verticalInput) * speed * Time.deltaTime;
+        displacement = transform.rotation * displacement;
+        Vector3 newPosition = transform.position + displacement;
+        for (int i =0; i < 3; ++i)
+        {
+            newPosition[i] = Mathf.Clamp(newPosition[i], -halfExtents[i], halfExtents[i]);
+        }
+        transform.position = newPosition;
+    }
+}
